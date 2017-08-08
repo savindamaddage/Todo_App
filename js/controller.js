5556e16f387b47caade8a89827445bca20a6d7fd
savindamaddage <savindak@salpo.com>
2017-08-08 17:20:18 +0530

    set todo list storage as browser's localstorage

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,15 @@
 var app = angular.module('myApp', []); 
 app.controller('todoCtrl', function($scope) {
     $scope.todoList = [];
+
+    $scope.todoList = JSON.parse(localStorage.getItem("todo_list"));
+
     var i = 0;
     $scope.todoAdd = function() {
         i++;
         $scope.todoList.push({todoText:$scope.todoInput, done:false, id:'list'+ i});
         $scope.todoInput = "";
+        localStorage.setItem("todo_list", JSON.stringify($scope.todoList));
     };
 
     $scope.remove = function(id) {
@@ -14,6 +18,7 @@ app.controller('todoCtrl', function($scope) {
         angular.forEach(oldList, function(x) {
             if (x.id!=id) $scope.todoList.push(x);
         });
+        localStorage.setItem("todo_list", JSON.stringify($scope.todoList));
     };
 
     $scope.removeMaked = function() {
@@ -22,5 +27,10 @@ app.controller('todoCtrl', function($scope) {
         angular.forEach(oldList, function(x) {
             if (!x.done) $scope.todoList.push(x);
         });
+        localStorage.setItem("todo_list", JSON.stringify($scope.todoList));
+    };
+
+    $scope.mark = function() {
+        localStorage.setItem("todo_list", JSON.stringify($scope.todoList));
     };
 });
