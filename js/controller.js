13133268f9524f15b93ed9ca3c013438cf0f58ce
savindamaddage <savindak@salpo.com>
2017-08-08 18:31:11 +0530

    fixed list item id error

--- js/controller.js
+++ js/controller.js
@@ -2,35 +2,40 @@ var app = angular.module('myApp', []);
 app.controller('todoCtrl', function($scope) {
     $scope.todoList = [];
 
+    //get array from local storage
     $scope.todoList = JSON.parse(localStorage.getItem("todo_list"));
 
-    var i = 0;
+    var i = $scope.todoList[$scope.todoList.length-1].idNo;
+
+    //add item
     $scope.todoAdd = function() {
         i++;
-        $scope.todoList.push({todoText:$scope.todoInput, done:false, id:'list'+ i});
+        $scope.todoList.push({todoText:$scope.todoInput, done:false, id:'list'+ i, idNo: i});
         $scope.todoInput = "";
-        localStorage.setItem("todo_list", JSON.stringify($scope.todoList));
+        localStorage.setItem("todo_list", JSON.stringify($scope.todoList)); //reset loacal storage
     };
 
+    //remove list item
     $scope.remove = function(id) {
         var oldList = $scope.todoList;
         $scope.todoList = [];
         angular.forEach(oldList, function(x) {
             if (x.id!=id) $scope.todoList.push(x);
         });
-        localStorage.setItem("todo_list", JSON.stringify($scope.todoList));
+        localStorage.setItem("todo_list", JSON.stringify($scope.todoList)); //reset loacal storage
     };
 
+    //remove selected items
     $scope.removeMaked = function() {
         var oldList = $scope.todoList;
         $scope.todoList = [];
         angular.forEach(oldList, function(x) {
             if (!x.done) $scope.todoList.push(x);
         });
-        localStorage.setItem("todo_list", JSON.stringify($scope.todoList));
+        localStorage.setItem("todo_list", JSON.stringify($scope.todoList)); //reset loacal storage
     };
 
     $scope.mark = function() {
-        localStorage.setItem("todo_list", JSON.stringify($scope.todoList));
+        localStorage.setItem("todo_list", JSON.stringify($scope.todoList)); //reset loacal storage
     };
 });
